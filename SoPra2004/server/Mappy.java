840eb89d2384015a8c35c9d9dd192d8b6693ecbc
fkubis <fkubis>
2004-12-17 17:37:06 +0000

    eclipse juhe

--- SoPra2004/server/Mappy.java
+++ SoPra2004/server/Mappy.java
@@ -6,8 +6,16 @@
  */
 package server;
 
-import java.awt.Point;
+/**
+ * @author ba008959
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.awt.Point;
+import java.awt.Dimension;
 
 import data.*;
 
@@ -16,19 +24,32 @@ public class Mappy {
 		
 	}
 	
-	public BufferedImage getMap(Point start, int type){
-		BufferedImage img = null;
+	public Image getMap(){
+		Image img=null;
 		DBValues myDBValues = new DBValues();
+		Point point = new Point(0, 0);
+		int type = 1;
 		try{
-			 img = myDBValues.getImage(start, type);
+			 img = myDBValues.getImage(point, type);
 		}
 		catch (Exception e){
 			System.err.println("koi bildle");
 		}
 		
-		if (img!=null) {
-			System.out.println("Bild wird übergeben...");
-		}
+		if (img!=null)System.out.println("Bild wird übergeben...");
 		return img;
+		
+	}
+	
+	public BufferedImage getMapDemo() {
+		System.err.println("getMapDemo");
+				
+		BufferedImage bI;
+				
+		Map myMap = new Map();
+		bI = myMap.generateMap(new Dimension(), new Point(), (double)100);
+		
+		System.err.println("Ende getMapDemo");
+		return bI;
 	}
 }
