be96670f625e2625d129cd83f0f3f07ae59ab0a6
drrsatzteil <drrsatzteil>
2004-12-17 18:54:16 +0000

    Unilogo und Schliessen-Button hinzugefügt. Noch keine Funktionalität

--- SoPra2004/client/AboutDialog.java
+++ SoPra2004/client/AboutDialog.java
@@ -6,13 +6,38 @@
  */
 package client;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class AboutDialog extends JDialog {
 	
+	private ImageIcon uniIcon;
+	private JLabel uniLabel;
+	private JLabel team;
+	private JButton close;
 	public AboutDialog(JFrame parent){
 		super(parent, "Über Mappy", true);
+		Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
+		int xPos = (screenDimension.width - this.getSize().width)/2;
+		int yPos = (screenDimension.height - this.getSize().height)/2;
+		this.setLocation(xPos, yPos);
+		setResizable (false);
+		initComponents();
+	}
+	private void initComponents(){
+		uniIcon = new ImageIcon ("uniba.gif");
+		uniLabel = new JLabel (uniIcon);
+		getContentPane().add(uniLabel, BorderLayout.NORTH);
+		close = new JButton ("Schliessen");
+		getContentPane().add(close);
 		pack();
+		setVisible (true);
 	}
 }
