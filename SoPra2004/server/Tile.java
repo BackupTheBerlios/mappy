82156b01a7096bb2be7f75f41503a09421c38dab
drrsatzteil <drrsatzteil>
2004-12-21 13:47:59 +0000

    Hauptsächlich die getSize eingefügt. lines und getLines werden vorraussichtlich nie gebraucht

--- SoPra2004/server/Tile.java
+++ SoPra2004/server/Tile.java
@@ -7,12 +7,11 @@ package server;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Point;
-
 import javax.swing.ImageIcon;
 
 /**
  * @author fkubis
- * $Id: Tile.java,v 1.3 2004/12/18 19:26:48 fkubis Exp $
+ * $Id: Tile.java,v 1.4 2004/12/21 13:47:59 drrsatzteil Exp $
  */
 public class Tile {
 	private int id;
@@ -20,44 +19,40 @@ public class Tile {
 	private Point to;
 	private Dimension dim;
 	private byte[] data;
-	
-	private boolean hasImage = false;
 	private ImageIcon image;
+	
+	public Tile(){
+	}
 	
 	public Tile(int id, int XFrom, int XTo, int YFrom, int YTo, byte[] gif) {		
 		this.id	= id;
 		from 	= new Point(XFrom, YFrom);
 		to 		= new Point(XTo, YTo);
+		dim = new Dimension(XFrom + XTo, YFrom + YTo);
 		data 	= gif;
-				
+		
 		try {
 			createImage();
 		} catch (NoImageException e) {
 			System.err.println("createImage der Tile: " + e.getMessage());
 		}
-		
-		System.out.println("EIN NEUES TILE WURDE GEBOREN! EIN HOCH AUF DIE KÜHE!");
 	}
 	
-	private void createImage() throws NoImageException {
-		image = new ImageIcon();
-		
+	private void createImage() throws NoImageException {		
 		if (data != null) {
 			image = new ImageIcon(data);
-			hasImage = true;
-			    		
-    		/*	
-			BufferedImage bI = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB);    			    			
-			bI = ImageIO.read( res.getUnicodeStream("Data") );
-			return bI;
-			*/
 		} else {
 			throw new NoImageException("Stream aus der DB ist null");
 		}		
 	}
 	
 	public boolean hasImage() {
-		return hasImage;
+		if(image != null){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 	
 	public Image getImage() {
@@ -69,6 +64,9 @@ public class Tile {
 	 */
 	public void setImage(ImageIcon icon) {
 		image = icon;
+	}
+	public Dimension getSize(){
+		return dim;
 	}
 
 }
