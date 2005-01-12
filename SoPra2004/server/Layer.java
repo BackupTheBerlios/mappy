04c73e248681145cf128f2c9fdca9def8ba23eeb
drrsatzteil <drrsatzteil>
2005-01-12 21:31:25 +0000

    *** empty log message ***

--- SoPra2004/server/Layer.java
+++ SoPra2004/server/Layer.java
@@ -8,11 +8,14 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
+import java.awt.image.ByteLookupTable;
+import java.awt.image.LookupOp;
+
 import data.DBValues;
 
 /**
  * @author fkubis
- * $Id: Layer.java,v 1.14 2005/01/12 21:23:06 drrsatzteil Exp $
+ * $Id: Layer.java,v 1.15 2005/01/12 21:31:25 drrsatzteil Exp $
  */
 public class Layer {
 	private BufferedImage map;
@@ -61,8 +64,24 @@ public class Layer {
 	/**
 	 * @param color
 	 */
-	public void setColor(Color color) {
-		
+	public void setColor(Color color){
+		byte cR[] = new byte[256];
+	    byte cG[] = new byte[256];
+	    byte cB[] = new byte[256];
+	    byte cA[] = new byte[256];
+							 
+	    cR[0]= (byte)color.getRed();
+	    cG[0]= (byte)color.getGreen();
+	    cB[0]= (byte)color.getBlue();
+	    cA[255]= (byte)255;
+	    ByteLookupTable blut = new ByteLookupTable(0, new byte [][] {cR, cG, cB, cA}); 
+	    LookupOp lop = new LookupOp (blut, null);
+	    //BufferedImage test = new BufferedImage (500, 500, BufferedImage.TRANSLUCENT);
+	    //test.getGraphics().drawImage(BufferedImageArray[i],0,0,null);
+	    //BufferedImageArray[i] = test;
+	    lop.filter(map, map);
+	    //System.out.println (BufferedImageArray[i]);
+	    //System.out.println (test);
 	}
 
 }
