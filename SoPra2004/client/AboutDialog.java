ef9cc2aafb9243ad345fe7982c8178d87e899717
flocente <flocente>
2005-01-17 17:55:27 +0000

    Wir haben Ihn: Der neue aboutDialog *LOL*

--- SoPra2004/client/AboutDialog.java
+++ SoPra2004/client/AboutDialog.java
@@ -8,7 +8,10 @@ package client;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -21,22 +24,32 @@ public class AboutDialog extends JDialog {
 	private ImageIcon uniIcon;
 	private JLabel uniLabel;
 	private JLabel team;
+	private JLabel mappyVersion;
 	private JButton close;
 	public AboutDialog(JFrame parent){
 		super(parent, "Über Mappy", true);
 		Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
-		int xPos = (screenDimension.width - this.getSize().width)/2;
-		int yPos = (screenDimension.height - this.getSize().height)/2;
+		int xPos = (screenDimension.width - this.getSize().width)/2-50;
+		int yPos = (screenDimension.height - this.getSize().height)/2-100;
 		this.setLocation(xPos, yPos);
 		setResizable (false);
 		initComponents();
 	}
 	private void initComponents(){
-		uniIcon = new ImageIcon ("uniba.gif");
+		uniIcon = new ImageIcon ("aboutlogo.gif");
 		uniLabel = new JLabel (uniIcon);
-		getContentPane().add(uniLabel, BorderLayout.NORTH);
+		getContentPane().add(uniLabel, BorderLayout.CENTER);
+		mappyVersion = new JLabel("<html><p/<p/Endlich habe ich es geschafft.<p/Wir haben einen neuen about Dialog.<p/" +
+				"Unsere aktuelle Version ist: 1.3<p/<p/</html>" );
+		mappyVersion.setFont(new Font("Verdana", Font.PLAIN, 11));
+		getContentPane().add(mappyVersion, BorderLayout.NORTH);
 		close = new JButton ("Schliessen");
-		getContentPane().add(close);
+		getContentPane().add(close, BorderLayout.SOUTH);
+		close.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				dispose();
+			}
+		});
 		pack();
 		setVisible (true);
 	}
