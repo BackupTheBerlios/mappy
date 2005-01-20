d81e271ce3b62c97a319977318180cc01d09c07f
drrsatzteil <drrsatzteil>
2005-01-20 01:17:57 +0000

    Die Exception die nun vom AudioPlayer geworfen wird

---
+++ SoPra2004/client/SoundDisabledException.java
@@ -0,0 +1,23 @@
+/*
+ * Created on 20.01.2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package client;
+
+/**
+ * @author DrRSatzteil
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class SoundDisabledException extends Exception{
+	public SoundDisabledException(){
+		super();
+	}
+
+	public SoundDisabledException(String s){
+		super(s);
+	}
+}
